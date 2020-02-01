20323dd9d8e63f92ae92750df92a10b182452185
Emilio Araos <ea3dev@gmail.com>
2020-01-31 22:21:37 -0700

    updated grid

--- parabola.py
+++ parabola.py
@@ -2,7 +2,7 @@ import tkinter
 
 
 def parabola(x):
-    y = x * x
+    y = x * x / 25
     return y
 
 
@@ -11,9 +11,9 @@ def draw_axes(canvas):
     x_origin = canvas.winfo_width() / 2
     y_origin = canvas.winfo_height() / 2
     canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
-    canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
-    canvas.create_line(0, y_origin, 0, -y_origin, fill="black")
-
+    canvas.create_line(-x_origin, 0, x_origin, 0, fill="white")
+    canvas.create_line(0, y_origin, 0, -y_origin, fill="white")
+    print(locals())
 def plot(canvas, x, y):
     canvas.create_line(x,y, x + 1, y + 1, fill="red")
 
@@ -24,13 +24,18 @@ mainWindow = tkinter.Tk()
 
 mainWindow.title("Parabola")
 mainWindow.geometry("640x480")
-canvas = tkinter.Canvas(mainWindow, width=1200, height=980)
+canvas = tkinter.Canvas(mainWindow, width=320, height=480, background="black")
 canvas.grid(row=0, column=0)
 
+canvas2 = tkinter.Canvas(mainWindow, width=320, height=480, background="pink")
+canvas2.grid(row=0, column=1)
+
+print(repr(canvas), repr(canvas2))
 draw_axes(canvas)
+draw_axes(canvas2)
 
 for x in range(-100, 100):
     y = parabola(x)
-    plot(canvas, x, y)
+    plot(canvas, x, -y)
 
 mainWindow.mainloop()
